89e1009cc40893aa17afe94ee87afd2fb6dc1f5b
rvst312 <115166784+rvst312@users.noreply.github.com>
2024-03-03 21:59:17 +0100

    img

--- app/components/HeroHome.jsx
+++ app/components/HeroHome.jsx
@@ -2,7 +2,7 @@ export function HeroHome(){
 
     return(
         <div className="hero-wrapper">
-            <img src="/img/Hero.png" alt="" />
+            <img src="https://cdn.shopify.com/s/files/1/0822/2569/3009/files/hero.png?v=1709499538" alt="" />
         </div>
     );
 }
