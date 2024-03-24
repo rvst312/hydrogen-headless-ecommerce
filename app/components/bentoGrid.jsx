64d459f378370cdc0bfac58f891a20cbb546109f
rvst312 <115166784+rvst312@users.noreply.github.com>
2024-03-24 10:50:20 +0100

    new

---
+++ app/components/bentoGrid.jsx
@@ -0,0 +1,27 @@
+export function BentoGrid() {
+
+    return (
+        <div className="bento-wrapper">
+            <div className="top">
+                <div className="left">
+                    <img src="" alt="" />
+                </div>
+                <div className="center">
+                    <img src="" alt="" />
+                </div>
+                <div className="right">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="left">
+                    <img src="" alt="" />
+                </div>
+                <div className="right">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div> 
+    );
+}
+
